88302335f3092ed58a0d474e3936ab47327381e2
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-07-06 21:19:16 -0500

    feat: set up role microservice

---
+++ src/main/java/pe/edu/pucp/msrole/MsRoleApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/msrole/MsRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
